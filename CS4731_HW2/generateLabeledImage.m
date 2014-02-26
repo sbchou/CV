0cc6e8d9f24b8627bc105a811c26ba847911a467
sophie <sbchou@gmail.com>
2014-02-26 18:18:52 -0500

    part 1a

--- CS4731_HW2/generateLabeledImage.m
+++ CS4731_HW2/generateLabeledImage.m
@@ -1,1 +1,13 @@
 function labeled_img = generateLabeledImage(gray_img, threshold)
+
+img = imread(gray_img);
+imshow(img); title('Original Image');
+
+% Convert the image into a binary image by applying a threshold
+%threshold = 0.5;
+
+bw_img = im2bw(img, threshold);
+imshow(bw_img); title('BW IMAGE');
+
+labeled_img = bwlabel(bw_img, 4);
+m = max(labeled_img(:))

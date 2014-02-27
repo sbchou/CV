e0bd45cbca39e9f1bcf0b0d3c00551df1bcca94d
sophie <sbchou@gmail.com>
2014-02-27 00:31:09 -0500

    handcode img2bw

--- CS4731_HW2/generateLabeledImage.m
+++ CS4731_HW2/generateLabeledImage.m
@@ -1,13 +1,21 @@
 function labeled_img = generateLabeledImage(gray_img, threshold)
+    %threshold of 0.5 works best
 
-img = imread(gray_img);
-imshow(img); title('Original Image');
+    img = imread(gray_img);
+    imshow(img); title('Original Image');
 
-% Convert the image into a binary image by applying a threshold
-%threshold = 0.5;
+    %bw_img = im2bw(img, threshold);
+    bw_img = zeros(size(img));
+    ind = find(img > 255 * threshold);
+    bw_img(ind) = img(ind);
+    
+    imshow(bw_img); title('BW IMAGE');
 
-bw_img = im2bw(img, threshold);
-imshow(bw_img); title('BW IMAGE');
+    labeled_img = bwlabel(bw_img, 4);
+    %for debugging 
+    m = max(labeled_img(:))
+    % display with different colors (for debug)
+    RGB = label2rgb(labeled_img);
+    imshow(RGB)
 
-labeled_img = bwlabel(bw_img, 4);
-m = max(labeled_img(:))
+end
